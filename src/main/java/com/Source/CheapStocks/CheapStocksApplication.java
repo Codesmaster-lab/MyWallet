fd5fd9768dd3fc57cdf15ecb3539e61a8bd265b5
Codesmaster-Lab <codesmaster404@gmail.com>
2022-01-24 14:59:14 +0530

    [ADD] spring boot app initialized

---
+++ src/main/java/com/Source/CheapStocks/CheapStocksApplication.java
@@ -0,0 +1,13 @@
+package com.Source.CheapStocks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CheapStocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CheapStocksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Source/CheapStocks/CheapStocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Source.CheapStocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CheapStocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
