a92a26e04ceb03ce8edb7d0806fae88f0f9edb35
Codesmaster-Lab <codesmaster404@gmail.com>
2022-02-21 19:19:43 +0530

    [ADD] CrossOrigin added and get axios implemented

--- frontend/src/Components/FundsList.jsx
+++ frontend/src/Components/FundsList.jsx
@@ -1,31 +1,29 @@
 import React from 'react';
 import axios from 'axios';
-import { render } from '@testing-library/react';
 
-export default class FundsList extends React.Components {
-        state = {
-                funds: []
-        }
+export default class FundsList extends React.Component {
+  state = {
+    funds: []
+  }
 
-        componentDidMount() {
-                axios.get('http://localhost:8080/api/getfunds').then(resp => {
-                        const funds = resp.data;
-                        this.setState({ funds })
-                })
-        }
-        render() {
-                return (
-                        <div>
-                                <ul>
-              {
+  componentDidMount() {
+    axios.get(`http://localhost:8080/api/getfunds`)
+      .then(res => {
+        const funds = res.data;
+        this.setState({ funds });
+      })
+  }
+
+  render() {
+    return (
+      <ul>
+        {
           this.state.funds
             .map(fundelement =>
-              <li key={fundelement.id}>{fundelement.name}</li>
+              <li key={fundelement.id}>{fundelement.id}   {fundelement.name}   {fundelement.total_inp}   {fundelement.savings1}   {fundelement.savings2}    {fundelement.doe}</li>
             )
         }
       </ul>
-                        </div>
-                );
-        }
+    )
+  }
 }
-
